00169623d7554c100b74b191738b2a17233d97b5
Решетников Евгений Константинович <ekreshetnikov@edu.hse.ru>
2023-04-16 15:33:02 +0500

    Add project files.

---
+++ PassManager/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private ILogger<AuthController> _logger;
+
+        private IConfiguration _config;
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetData()
+        {
+            _logger.LogInformation("Gotcha!");
+            return Ok();
+        }
+    }
+}
